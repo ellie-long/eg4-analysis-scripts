aac8f2a8df5691bfd91ceb21bbdc610a6f0392e7
ellie-long <ellie@jlab.org>
2015-02-03 13:54:07 -0500

    First working version of run_check.cpp

--- run_check.cpp
+++ run_check.cpp
@@ -35,7 +35,13 @@ void run_check(){
 		cin >> checkAll;
 
 	}
-	else {e_beam=1.1; target="nh3"; checkAll="y"; maxSector = 2;}
+	else 
+	{
+		e_beam=1.1; 
+		target="nh3"; 
+		checkAll="y"; 
+//		maxSector = 2;
+	}
 
 	if (e_beam==1.1) {e_beamStr = "1p1gev/";}
 	else if (e_beam==1.3) {e_beamStr = "1p3gev/";}
@@ -85,7 +91,7 @@ void run_check(){
 void check_run(int runNumber, double e_beam, TString targetStr, TString target, TString filePrefix, int sector, bool debug)
 {
 	cout << "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv" << endl;
-	cout << "Running check_run for Run # " << runNumber << "..." << endl;
+	cout << "Running check_run for Run # " << runNumber << ", Sector " << sector << "..." << endl;
 	TString fileName;
 	TChain* chain = new TChain("h10");
 	bool fileExists = false;
@@ -143,8 +149,8 @@ void check_run(int runNumber, double e_beam, TString targetStr, TString target, 
 		pad2  =  new  TPad("pad2","pad2",0.3333,0.6333,0.6666,0.9500,0,0,0);
 		pad3  =  new  TPad("pad3","pad3",0.6666,0.6333,1.0000,0.9500,0,0,0);
 		pad4  =  new  TPad("pad4","pad4",0.0000,0.3166,0.3333,0.6333,0,0,0);
-		pad5a =  new  TPad("pad5a","pad5a",0.3333,0.4549,0.6666,0.6333,0,0,0);
-		pad5b =  new  TPad("pad5b","pad5b",0.3333,0.3166,0.6666,0.4549,0,0,0);
+		pad5a =  new  TPad("pad5a","pad5a",0.3333,0.4749,0.6666,0.6333,0,0,0);
+		pad5b =  new  TPad("pad5b","pad5b",0.3333,0.3166,0.6666,0.4749,0,0,0);
 		pad6  =  new  TPad("pad6","pad6",0.6666,0.3166,1.0000,0.6333,0,0,0);
 		pad7  =  new  TPad("pad7","pad7",0.0000,0.0000,0.3333,0.3166,0,0,0);
 		pad8  =  new  TPad("pad8","pad8",0.3333,0.0000,0.6666,0.3166,0,0,0);
@@ -155,66 +161,115 @@ void check_run(int runNumber, double e_beam, TString targetStr, TString target, 
 
 	
 		pad0->cd();
-		TPaveLabel *titleBox = new TPaveLabel(0,0,1,1,title);
+		TPaveLabel *titleBox = new TPaveLabel(0.1,0.1,0.9,0.9,title);
 		titleBox->Draw();
+		titleBox->SetFillColor(kWhite);
 		pad0->Update();
 
 		pad1->cd();
+		TString titleRaster = "Raster x:y for Run # ";
+		titleRaster += runNumber;
+		cout << "Drawing " << titleRaster << "..."  << endl;
+		chain->Draw("raster_x:raster_y>>raster(100,2000,6000,100,2000,6000)", cuts, "");
+		raster->SetTitle(titleRaster);
+		raster->SetStats(kFALSE);
+		raster->Draw("COLZ");
+		pad1->Update();
+
+		pad2->cd();
 		TString titleTarget = "Vertex z for Run # ";
 		titleTarget += runNumber;
 		cout << "Drawing Vertex z..." << endl;
 		chain->Draw("dc_vz>>vertZ(400,-800,800)", cuts, "");
 		vertZ->SetTitle(titleTarget);
 		vertZ->Draw();
-		pad1->Update();
-
-		pad2->cd();
-		TString titleQ2 = "Q2 for Run # ";
-		titleQ2 += runNumber;
-		cout << "Drawing " << titleQ2 << "..." << endl;
-		TString drawQ2 = "4*";
-		drawQ2 += e_beam;
-		drawQ2 += "*p*(sin(dc_ccth*3.14159/180/2)^2)>>histQ2(400,0,6)";
-		chain->Draw(drawQ2, cuts, "");
-		histQ2->SetTitle(titleQ2);
-	//	histQ2->SetStats(kFALSE);
-		histQ2->Draw();
-		pad2->Update(); 
+		pad2->Update();
 
 		pad3->cd();
+		TString titleVThPh = "Vertex th:ph for Run # ";
+		titleVThPh += runNumber;
+		cout << "Drawing " << titleVThPh << "..." << endl;
+		chain->Draw("atan(dc_vx/dc_vy)*180/3.14159:acos(dc_vz/sqrt(dc_vx*dc_vx+dc_vy*dc_vy+dc_vz*dc_vz))*180/3.14159>>vThPh(300,0,180,300,-90,90)", cuts, "");
+		vThPh->SetTitle(titleVThPh);
+		vThPh->SetStats(kFALSE);
+		vThPh->Draw("COLZ");
+		pad3->Update();
+
+		pad4->cd();
+		TString titlebeamE = "beamE for Run # ";
+		titlebeamE += runNumber;
+		cout << "Drawing " << titlebeamE << "..." << endl;
+		TString drawbeamE = "";
+		drawbeamE += e_beam;
+		drawbeamE += ">>beamE(100,0,4)";
+		chain->Draw(drawbeamE, cuts, "");
+		beamE->SetTitle(titlebeamE);
+	//	beamE->SetStats(kFALSE);
+		beamE->Draw();
+		pad4->Update();
+
+		pad5a->cd();
+		TString titleRThNphe = "Reconstructed Theta:nphe for Run # ";
+		titleRThNphe += runNumber;
+		cout << "Drawing " << titleRThNphe << "..."  << endl;
+		chain->Draw("dc_ccth:nphe>>rThNphe(100,0,500,100,0,60)", cuts, "");
+		rThNphe->SetTitle(titleRThNphe);
+		rThNphe->SetStats(kFALSE);
+		rThNphe->Draw("COLZ");
+		pad5a->Update();
+
+		pad5b->cd();
+		TString titleRPhNphe = "Reconstructed Phi:nphe for Run # ";
+		titleRPhNphe += runNumber;
+		cout << "Drawing " << titleRPhNphe << "..."  << endl;
+		chain->Draw("dc_ccph:nphe>>rPhNphe(100,0,500,100,-30,30)", cuts, "");
+		rPhNphe->SetTitle(titleRPhNphe);
+		rPhNphe->SetStats(kFALSE);
+		rPhNphe->Draw("COLZ");
+		pad5b->Update();
+
+		pad6->cd();
 		TString titleNu = "Nu for Run # ";
 		titleNu += runNumber;
 		cout << "Drawing " << titleNu << "..." << endl;
 		TString drawNu = "";
 		drawNu += e_beam;
-		drawNu += "-p>>histNu(100,-1,4)";
+		drawNu += "-p>>histNu(100,-0.2,1.1)";
 		chain->Draw(drawNu, cuts, "");
 		histNu->SetTitle(titleNu);
 	//	histNu->SetStats(kFALSE);
 		histNu->Draw();
-		pad3->Update();
-
-		pad4->cd();
-		TString titledp = "dE/dx for Run # ";
-		titledp += runNumber;
-		cout << "Drawing " << titledp << "..." << endl;
-		chain->Draw("edep>>dEdx(400,0,100)", cuts, "");
-		dEdx->SetTitle(titledp);
-		dEdx->Draw();
-		pad4->Update();
-
-		pad5a->cd();
-		TString titleThetaPhi = "Theta and Phi for Run # ";
-		titleThetaPhi += runNumber;
-		cout << "Drawing " << titleThetaPhi << "..."  << endl;
-		chain->Draw("dc_ccth:dc_ccph>>thPh(100,-60,60,100,0,60)", cuts, "");
-		thPh->SetTitle(titleThetaPhi);
-		thPh->SetStats(kFALSE);
-		thPh->Draw("COLZ");
-		pad5a->Update();
+		pad6->Update();
 
 		pad7->cd();
-		pad7->SetLogy();
+		TString titleW2 = "W^2 for Run # ";
+		titleW2 += runNumber;
+		cout << "Drawing " << titleW2 << "..." << endl;
+		TString drawW2 = "0.938*0.938+2*0.938*(";
+		drawW2 += e_beam;
+		drawW2 += "-p)-4*";
+		drawW2 += e_beam;
+		drawW2 += "*p*(sin(dc_ccth*3.14159/180/2)^2)>>histW2(100,0,4)";
+		chain->Draw(drawW2, cuts, "");
+		histW2->SetTitle(titleW2);
+	//	histW2->SetStats(kFALSE);
+		histW2->Draw();
+		pad7->Update();
+
+		pad8->cd();
+		TString titleQ2 = "Q^2 for Run # ";
+		titleQ2 += runNumber;
+		cout << "Drawing " << titleQ2 << "..." << endl;
+		TString drawQ2 = "4*";
+		drawQ2 += e_beam;
+		drawQ2 += "*p*(sin(dc_ccth*3.14159/180/2)^2)>>histQ2(400,0,0.5)";
+		chain->Draw(drawQ2, cuts, "");
+		histQ2->SetTitle(titleQ2);
+		histQ2->Draw();
+		pad8->Update(); 
+
+		pad9->cd();
+//		pad9->SetLogy();
 		TString titlex = "Bjorken x ";
 		titlex += " (x=Q^2/[2m*nu]) for Run # ";
 		titlex += runNumber;
@@ -223,14 +278,13 @@ void check_run(int runNumber, double e_beam, TString targetStr, TString target, 
 		drawx += e_beam;
 		drawx += "*p*(sin(dc_ccth*3.14159/180/2)^2)/(2*0.938*(";
 		drawx += e_beam;
-		drawx += "-p))>>histx(400,0,6)";
-	//	TH1F *histx = new TH1F("histx",titlex,bins,xmin,xmax);
-	//	histx->Sumw2();
+		drawx += "-p))>>histx(400,0,3)";
 		chain->Draw(drawx, cuts, "");
 		histx->SetTitle(titlex);
 		histx->Draw();
-		pad7->Update();
-		
+		pad9->Update();
+
+	
 		TString imagename = "./Run_Checks/Run_Check_for_Sector_";
 		imagename += sector;
 		imagename += "_Run_";
@@ -239,6 +293,11 @@ void check_run(int runNumber, double e_beam, TString targetStr, TString target, 
 		c1->Print(imagename);
 	}
 	else {cout << "There is no file for Run # " << runNumber << endl;}
+
+	TString exitst;
+//	cout << "Are you ready to finish?" << endl;
+//	cin >> exitst;
+
 	
 	cout << "Finished check_run for Run # " << runNumber << "!" << endl;
 	cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" << endl;
